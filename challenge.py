87c2cd7b7e3b173163f4ba2349fc3fe04bc3d8dc
SecretJuice <connor.m.robb33@gmail.com>
2023-03-09 19:09:04 -0700

    Added docstrings and comments

--- challenge.py
+++ challenge.py
@@ -55,7 +55,7 @@ class Word():
         for i in range(len(complete_word)):
             self.partial += "_"
     
-    def __str__(self):
+    def __str__(self):          # I'm choosing to fulfill requirement 3 by immediately printing the partial string with the proper number of underscores
         string = "\n\n"
 
         for char in self.partial:
@@ -64,6 +64,8 @@ class Word():
         return string.upper()
     
     def CheckGuess(self, guess_char):
+        '''See if guess 'guess_char' is found in the word and replaces the underscore if so.
+        Returns bool for whether it found a match'''
 
         found_match = False
 
@@ -89,6 +91,8 @@ class Word():
         return found_match
     
     def CheckCompleteness(self):
+        '''Sees if all of the characters in the word have been guessed. Returns bool
+        for whether or not it does'''
 
         word_complete = self.complete == self.partial
 
@@ -96,6 +100,8 @@ class Word():
 
 
 def GameLoop(greeting_message):
+    '''Prints a greeting message "greeting_message" and then begins play
+    by initializing and starting the game'''
 
     print(greeting_message)
 
@@ -104,14 +110,17 @@ def GameLoop(greeting_message):
     
 
 def InitializeGame():
+    '''Calls the ResetGuesses and SelectWord functions'''
 
     ResetGuesses()
     SelectWord()
 
 def SelectWord():
+    '''Randomly selects a word from a list and set global var "current_word" to a
+    new Word object '''
 
     word_list = ["donut", "pizza", "anaconda", "supercalifragilisticexpialidocious",
-                 "office", "information", "technology", "foobar", "moroni", "cowboy"]
+                 "office", "information", "technology", "foo", "lehi", "boots"]
     
     global current_word
 
@@ -120,6 +129,7 @@ def SelectWord():
     current_word = Word(new_word_string)
 
 def ResetGuesses():
+    '''Resets all the global guess vars'''
 
     global num_correct_guesses
     global num_incorrect_guesses
@@ -130,13 +140,15 @@ def ResetGuesses():
     guess_chars = []
 
 def PlayGame():
+    '''Until the word is guessed, drive the gameplay by printing the gallows, Word string and guess data
+    and then taking a guess from the user input. When they win, restart or quit depending on user input'''
 
     playing = True
 
     while playing:
 
         PrintGallows(num_incorrect_guesses)
-        print(current_word)
+        print(current_word)                             
         PrintGuesses()
         AcceptGuess()
 
@@ -149,6 +161,7 @@ def PlayGame():
         GameLoop("Let's play again!")
 
 def PrintGuesses():
+    '''Display guess data, including incorrect characters and correct/incorrect guess numbers'''
 
     string = "\n"                                           #Add an extra line of space
 
@@ -167,6 +180,9 @@ def PrintGuesses():
     print(f"Guess Total: {num_correct_guesses + num_incorrect_guesses}  #Correct Guesses: {num_correct_guesses} -- #Incorrect Guesses: {num_incorrect_guesses}\n")
 
 def PrintGallows(num_guesses):
+    '''Builds the gallows based on the number of incorrect guesses "num_guesses" then
+    prints it line by line'''
+
 
     gallows_list = ["T----T",
                     "|    |",
@@ -182,11 +198,11 @@ def PrintGallows(num_guesses):
                   "|   / ",
                   "|   / \\   bruh"]
     
-    if num_guesses >= 1:
-        gallows_list[2] = guess_list[0]
-    if num_guesses == 2:
-        gallows_list[3] = guess_list[1]
-    if num_guesses == 3:
+    if num_guesses >= 1:                        # I'm sure there's a better way... but it works. 
+        gallows_list[2] = guess_list[0]     
+    if num_guesses == 2:    
+        gallows_list[3] = guess_list[1]         # Depending on how many guesses, replace the corresponding item in "gallows_list"
+    if num_guesses == 3:                        # with the correct element in "guess_list"
         gallows_list[3] = guess_list[2]
     if num_guesses >= 4:
         gallows_list[3] = guess_list[3]
@@ -203,6 +219,10 @@ def PrintGallows(num_guesses):
 
 
 def AcceptGuess():
+    '''Gets user input for their guess, then checks to see if it matches
+    the word. If so, increase global var "num_correct_guesses". If not,
+    append the character to the global list "guess_chars" and increment 
+    global var "num_incorrect_guesses"'''
 
     user_inputing = True
 
@@ -229,6 +249,9 @@ def AcceptGuess():
         num_incorrect_guesses += 1
 
 def WordIsGuessed():
+    '''Called at the end of each turn. Checks to see if "current_word" has been
+    completely guessed. If so, prints the completed word and guess information and
+    return True. If not, return False'''
 
     guessed = current_word.CheckCompleteness()
 
@@ -242,6 +265,9 @@ def WordIsGuessed():
         return False
     
 def UserRestart():
+    '''Gets input from the user on whether they'd like to restart or quit the game.
+    If they'd like to restart, return True. If they want to quit, print a goodbye message and 
+    return False'''
 
     response = input("Would you like to play again? (y: play again -- enter: quit)")
 
